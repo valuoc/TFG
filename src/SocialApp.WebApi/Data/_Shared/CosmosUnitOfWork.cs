ea35d7449d9e64fef44c3ac0cbb8a84afca1a33b
valuoc <vtortola@uoc.edu>
2025-05-05 00:51:27 +0200

    feat(UoW): Container 3.

--- src/SocialApp.WebApi/Data/_Shared/CosmosUnitOfWork.cs
+++ src/SocialApp.WebApi/Data/_Shared/CosmosUnitOfWork.cs
@@ -1,29 +1,9 @@
 using System.Linq.Expressions;
 using System.Net;
 using Microsoft.Azure.Cosmos;
-using SocialApp.WebApi.Data.Account;
 using SocialApp.WebApi.Features._Shared.Services;
 
 namespace SocialApp.WebApi.Data._Shared;
-
-public interface IUnitOfWork
-{
-    void Create<T>(T document)
-        where T:Document;
-    void Increment<T>(DocumentKey key, Expression<Func<T,int>> path, int increment = 1)
-        where T:Document;
-    void Set<T>(DocumentKey key, Expression<Func<T,object>> path, object value)
-        where T:Document;
-    void CreateOrUpdate<T>(T document)
-        where T:Document;
-    void Update<T>(T document)
-        where T:Document;
-    void Delete<T>(T document)
-        where T:Document;
-    void Delete<T>(DocumentKey key)
-        where T:Document;
-    Task SaveChangesAsync(OperationContext context);
-}
 
 public class CosmosUnitOfWork : IUnitOfWork
 {
