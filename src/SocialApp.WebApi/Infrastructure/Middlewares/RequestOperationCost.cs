ca63d1ef1cf3ce194fc0832ff5169f63dfe2ba98
valuoc <vtortola@uoc.edu>
2025-06-02 21:43:09 +0200

    fix(Cli): Put region urls in config.

---
+++ src/SocialApp.WebApi/Infrastructure/Middlewares/RequestOperationCost.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using SocialApp.WebApi.Features._Shared.Services;
+
+namespace SocialApp.WebApi.Infrastructure.Middlewares;
+
+public class RequestOperationCost
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestLog> _logger;
+
+    public RequestLog(RequestDelegate next, ILogger<RequestLog> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            await _next(context); // pass to next middleware
+            sw.Stop();
+            
+            var operationContext = context.RequestServices.GetRequiredService<OperationContext>();
+            context.Response.Headers.Append("X-OperationCharge", operationContext.OperationCharge.ToString());
+            _logger.LogInformation(
+                "HTTP {Method} {Path} => {StatusCode}, {Elapsed}ms, {Operation}RU",
+                context.Request.Method,
+                context.Request.Path,
+                context.Response.StatusCode,
+                sw.ElapsedMilliseconds,
+                operationContext.OperationCharge);
+        }
+        catch (Exception ex)
+        {
+            sw.Stop();
+            var operationContext = context.RequestServices.GetRequiredService<OperationContext>();
+            context.Response.Headers.Append("X-OperationCharge", operationContext.OperationCharge.ToString());
+            _logger.LogError(
+                "HTTP {Method} {Path} => {StatusCode}, {Elapsed}ms, {Operation}RU",
+                context.Request.Method,
+                context.Request.Path,
+                context.Response.StatusCode,
+                sw.ElapsedMilliseconds,
+                operationContext.OperationCharge);
+
+            throw; // rethrow to preserve behavior
+        }
+    }
+}
+
